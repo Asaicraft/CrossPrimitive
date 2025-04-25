60f0431bb121554f41b2b5c507801b8a55f89267
craft war <bay.asanov@mail.ru>
2025-04-26 05:33:30 +0600

    feat: add new behavior and transformation interfaces
    Introduced several new interfaces under the `CrossPrimitives.Behaviors` namespace to define common behaviors and transformations:
    
    - Added `IFixedUpdatable` and `IFixedUpdate` interfaces for fixed-timestep updates with a `FixedUpdate(double deltaTime)` method.
    - Added `IUpdatable` and `IUpdate` interfaces for per-frame updates with an `Update(double deltaTime)` method.
    - Added `IStart` and `IStartable` interfaces for initialization/start behavior, with `IStartable` exposing a `Start` property of type `IStart`.
    - Added `ITransform` and `ITransformable` interfaces for 2D transformations, with properties for `Position`, `GlobalPosition`, and `Scale` (`Vector2` type).
    
    Additionally, updated `CrossPrimitives.csproj` to fix formatting issues in the `<ProjectReference>` element for `CrossPrimitives.SourceGenerator`, improving XML readability.

---
+++ CrossPrimitives/Behaviors/IStartable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPrimitives.Behaviors;
+
+public interface IStartable
+{
+    public IStart Start
+    {
+        get;
+    }
+}
---
+++ CrossPrimitives/Behaviors/ITransform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPrimitives.Behaviors;
+
+public interface ITransform
+{
+    public Vector2 GlobalPosition
+    {
+        get; set;
+    }
+
+    public Vector2 Position
+    {
+        get; set;
+    }
+
+    public Vector2 Scale
+    {
+        get; set;
+    }
+}
