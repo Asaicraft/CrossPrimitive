13395cccaadfb8416e91556eac37e5ed96cc96bd
craft war <bay.asanov@mail.ru>
2025-04-26 15:02:09 +0600

    feat: update source generator setup for TransformAdapterGenerator
    Removed `<PackageType>` element with value `Analyzer` from `CrossPrimitives.GodotAdapterGenerator.csproj`, indicating a change in packaging strategy.
    
    Added `[Generator(LanguageNames.CSharp)]` attribute to `TransformAdapterGenerator` class, marking it as a C# source generator. Updated the class to implement the `IIncrementalGenerator` interface and added the `Initialize` method to register post-initialization output using `context.RegisterPostInitializationOutput`.

--- CrossPrimitives.GodotAdapterGenerator/TransformAdapterGenerator.cs
+++ CrossPrimitives.GodotAdapterGenerator/TransformAdapterGenerator.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 namespace CrossPrimitives.GodotAdapterGenerator;
 
+[Generator(LanguageNames.CSharp)]
 public sealed class TransformAdapterGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
